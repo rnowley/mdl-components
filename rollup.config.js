49817c783135cb7a8dfc1bba734da8fd9910767d
rnowley <nowley.r@gmail.com>
2016-12-22 14:10:24 +1300

    Set up rollup configuration.

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import eslint from 'rollup-plugin-eslint';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify';
+
+export default {
+    entry: 'src/scripts/main.js',
+	dest: 'build/js/mdl-components.min.js',
+    format: 'iife',
+    sourceMap: 'inline',
+    plugins: [
+        resolve({
+            jsnext: true,
+            main: true,
+            browser: true,
+        }),
+        commonjs(),
+        eslint({
+            exclude: [
+                'src/styles/**',
+            ]
+        }),
+        replace({
+            exclude: 'node_modules/**',
+            ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+        }),
+        (process.env.NODE_ENV === 'production' && uglify()),
+    ],
+};
+
