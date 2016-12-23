b9c664d277eec21385003a8c8f3d40aa2401374d
nowley.r <nowley.r@gmail.com>
2016-12-23 23:35:50 +1300

    Added new layout components.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,17 @@
-import {TransparentHeaderLayout, FixedDrawerLayout, LayoutContent, Navigation, NavigationLink} from './modules/layout';
+import {TransparentHeaderLayout, FixedDrawerLayout, FixedHeaderLayout, 
+        ScrollingHeaderLayout, WaterfallHeaderLayout, ScrollableTabLayout, LayoutContent, 
+        Navigation, NavigationLink, TabBar, Tab, TabPanel, PageContent} from './modules/layout';
 
 document.registerElement('mdl-transparent-header-layout', TransparentHeaderLayout);
 document.registerElement('mdl-fixed-drawer-layout', FixedDrawerLayout);
+document.registerElement('mdl-fixed-header-layout', FixedHeaderLayout);
+document.registerElement('mdl-scrolling-header-layout', ScrollingHeaderLayout);
+document.registerElement('mdl-waterfall-header-layout', WaterfallHeaderLayout);
+document.registerElement('mdl-scrollable-tab-layout', ScrollableTabLayout);
 document.registerElement('mdl-layout-content', LayoutContent);
 document.registerElement('mdl-navigation', Navigation);
 document.registerElement('mdl-navigation-link', NavigationLink);
+document.registerElement('mdl-tab-bar', TabBar);
+document.registerElement('mdl-tab', Tab);
+document.registerElement('mdl-tab-panel', TabPanel);
+document.registerElement('mdl-page-content', PageContent);
